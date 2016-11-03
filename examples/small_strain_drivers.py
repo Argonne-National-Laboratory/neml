1381c17ecd70965c79894f236f71963e4f02a0a5
Mark C. Messner <messner@anl.gov>
2016-11-03 17:15:09 -0500

    Small strain drivers done, will have to add extrapolation or something to the rate control one

---
+++ examples/small_strain_drivers.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('..')
+
+from neml import neml, volumetric, deviatoric, shear, drivers
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def example_strain(model, strain, T, t, nsteps):
+  """
+    Parameters:
+      model     material model
+      strain    target strain
+      T         constant temperature
+      t         time to take
+      nsteps    number of steps
+  """
+  de = strain / nsteps
+  dt = t / nsteps
+  
+  e = np.zeros((6,))
+  t = 0
+
+  driver = drivers.Driver_sd(model, verbose = True)
+  
+  print("Driving strain...")
+  for i in range(nsteps):
+    print(i+1)
+    e += de
+    t += dt
+
+    driver.strain_step(e, t, T)
+  
+  plt.plot(driver.strain[:,0], driver.stress[:,0], 'k-')
+  plt.plot(driver.strain[:,0], driver.stress[:,1], 'r-')
+  plt.plot(driver.strain[:,0], driver.stress[:,2], 'b-')
+
+  plt.plot(driver.strain[:,0], driver.stress[:,3], 'k--')
+  plt.plot(driver.strain[:,0], driver.stress[:,4], 'r--')
+  plt.plot(driver.strain[:,0], driver.stress[:,5], 'b--')
+  plt.show()
+
+
+def example_stress(model, stress, T, t, nsteps):
+  """
+    Parameters:
+      model     material model
+      stress    target stress
+      T         constant temperature
+      t         time to take
+      nsteps    number of steps
+  """
+  ds = stress / nsteps
+  dt = t / nsteps
+  
+  s = np.zeros((6,))
+  t = 0
+
+  driver = drivers.Driver_sd(model, verbose = True)
+  
+  print("Driving stress...")
+  for i in range(nsteps):
+    print(i+1)
+    s += ds
+    t += dt
+
+    driver.stress_step(s, t, T)
+  
+  plt.plot(driver.strain[:,0], driver.stress[:,0], 'k-')
+  plt.plot(driver.strain[:,0], driver.stress[:,1], 'r-')
+  plt.plot(driver.strain[:,0], driver.stress[:,2], 'b-')
+
+  plt.plot(driver.strain[:,0], driver.stress[:,3], 'k--')
+  plt.plot(driver.strain[:,0], driver.stress[:,4], 'r--')
+  plt.plot(driver.strain[:,0], driver.stress[:,5], 'b--')
+  plt.show()
+
+def example_rate(model, sdir, rate, T, dt, nsteps):
+  """
+    Parameters:
+      model     material model
+      sdir      stress direction
+      rate      strain rate
+      T         constant temperature
+      dt        time increment
+      nsteps    number of steps
+  """
+  t = 0
+
+  driver = drivers.Driver_sd(model, verbose = True)
+
+  print("Rate controlled...")
+  for i in range(nsteps):
+    print(i+1)
+    t += dt
+    driver.rate_step(sdir, rate, t, T)
+
+  plt.plot(driver.strain[:,0], driver.stress[:,0], 'k-')
+  plt.plot(driver.strain[:,0], driver.stress[:,1], 'r-')
+  plt.plot(driver.strain[:,0], driver.stress[:,2], 'b-')
+
+  plt.plot(driver.strain[:,0], driver.stress[:,3], 'k--')
+  plt.plot(driver.strain[:,0], driver.stress[:,4], 'r--')
+  plt.plot(driver.strain[:,0], driver.stress[:,5], 'b--')
+  plt.show()
+
+if __name__ == "__main__":
+  E = 200000.0
+  nu = 0.3
+
+  mu = E / (2 * (1.0 + nu))
+  K = E / (3 * (1 - 2 * nu))
+
+  vol_model = volumetric.VModelConstantK(K)
+  shear_model = shear.ConstantShearModulus(mu)
+  dev_model = deviatoric.LEModel(shear_model)
+  model = neml.SplitModel_sd(vol_model, dev_model)
+
+  example_strain(model, np.array([0.1,0,0,0,0,0]), 300.0, 10, 20)
+  example_stress(model, np.array([1000.0,0,0,0,0,0]), 300.0, 10, 50)
+  example_rate(model, np.array([1,0,0,0,0,0]), 1.0e-2, 300.0, 1.0e-3, 50)
+
---
+++ neml/drivers.py
@@ -0,0 +1,189 @@
+import numpy as np
+import numpy.linalg as la
+
+class Driver(object):
+  """
+    Superclass of all drivers, basically just sets up history and reports
+    results.
+  """
+  def __init__(self, model, verbose = False, rtol = 1.0e-6, atol = 1.0e-10,
+      miter = 20):
+    """
+      Parameters:
+        model       material model to play with
+
+      Optional:
+        verbose     verbose output
+        rtol        relative tolerance, where needed
+        atol        absolute tolerance, where needed
+        miter       maximum iterations, where needed
+    """
+    self.model = model
+    self.verbose = verbose
+    self.rtol = rtol
+    self.atol = atol
+    self.miter = miter
+
+    self.stress_int = [np.zeros((6,))]
+    self.stored_int = [self.model.init_store()]
+    self.T_int = [0.0]
+    self.t_int = [0.0]
+
+  @property
+  def stress(self):
+    return np.array(self.stress_int)
+
+  @property
+  def stored(self):
+    return np.array(self.stored_int)
+
+  @property
+  def history(self):
+    return self.stored[:,:self.model.nhist]
+
+  @property
+  def T(self):
+    return np.array(self.T_int)
+
+  @property
+  def t(self):
+    return np.array(self.t_int)
+
+
+class Driver_sd(Driver):
+  """
+    Superclass of generic small strain drivers, contains generic step methods.
+  """
+  def __init__(self, *args, **kwargs):
+    super(Driver_sd, self).__init__(*args, **kwargs)
+    self.strain_int = [np.zeros((6,))]
+
+  @property
+  def strain(self):
+    return np.array(self.strain_int)
+
+  def strain_step(self, e_np1, t_np1, T_np1):
+    """
+      Take a strain-controlled step
+
+      Parameters:
+        e_np1       next strain
+        t_np1       next time
+        T_np1       next temperature
+    """
+    s_np1, h_np1, A_np1 = self.model.update_sd(e_np1, self.strain_int[-1],
+        T_np1, self.T_int[-1], t_np1, self.t_int[-1], self.stress_int[-1],
+        self.stored_int[-1])
+    
+    self.strain_int.append(np.copy(e_np1))
+    self.stress_int.append(s_np1)
+    self.stored_int.append(h_np1)
+    self.T_int.append(T_np1)
+    self.t_int.append(t_np1)
+
+  def stress_step(self, s_np1, t_np1, T_np1):
+    """
+      Take a stress-controlled step
+
+      Parameters:
+        s_np1       next stress
+        t_np1       next time
+        T_np1       next temperature
+    """
+    def RJ(e):
+      s, h, A = self.model.update_sd(e, self.strain_int[-1],
+        T_np1, self.T_int[-1], t_np1, self.t_int[-1], self.stress_int[-1],
+        self.stored_int[-1])
+      R = s - s_np1
+      return R, A
+
+    e_np1 = newton(RJ, self.strain_int[-1], verbose = self.verbose,
+        rtol = self.rtol, atol = self.atol, miter = self.miter)
+
+    self.strain_step(e_np1, t_np1, T_np1)
+
+  def rate_step(self, sdir, erate, t_np1, T_np1):
+    """
+      Drive in a given stress direction at a prescribed strain rate, like
+      an actual "stress controlled" experiment.
+
+      Parameters:
+        sdir        stress direction
+        erate       strain rate (in the direction)
+        t_np1       next time
+        T_np1       next temperature
+    """
+    sdir /= la.norm(sdir)
+    dt = t_np1 - self.t_int[-1]
+    def RJ(x):
+      a = x[0]
+      e_inc = x[1:]
+      s, h, A = self.model.update_sd(self.strain_int[-1] + e_inc,
+          self.strain_int[-1],
+          T_np1, self.T_int[-1], t_np1, self.t_int[-1], self.stress_int[-1],
+          self.stored_int[-1])
+
+      R = np.zeros((7,))
+      J = np.zeros((7,7))
+      R[:6] = s - (sdir * a + self.stress_int[-1])
+      R[6] = np.dot(e_inc, sdir) / dt - erate
+
+      J[:6,0] = -sdir
+      J[:6,1:] = A
+      J[6,0] = 0.0
+      J[6,1:] = sdir / dt
+
+      return R, J
+    
+    x0 = np.zeros((7,))
+    x0[0] = 1.0
+    x0[1:] = sdir / 1000.0
+    x = newton(RJ, x0, verbose = self.verbose,
+        rtol = self.rtol, atol = self.atol, miter = self.miter)
+    e_np1 = x[1:]
+
+    self.strain_step(e_np1, t_np1, T_np1)
+
+
+class MaximumIterations(RuntimeError):
+  def __init__(self):
+    super(MaximumIterations, self).__init__("Exceeded the maximum allowed iterations!")
+
+def newton(RJ, x0, verbose = False, rtol = 1.0e-6, atol = 1.0e-10, miter = 20):
+  """
+    Manually-code newton-raphson so that I can output convergence info, if
+    requested.
+
+    Parameters:
+      RJ        function return the residual + jacobian
+      x0        initial guess
+
+    Optional:
+      verbose   verbose output
+  """
+  R, J = RJ(x0)
+  nR = la.norm(R)
+  nR0 = nR
+  x = x0
+  
+  i = 0
+
+  if verbose:
+    print("Iter.\tnR\t\tnR/nR0")
+    print("%i\t%e\t%e\t" % (i, nR, nR / nR0))
+
+  while (nR > rtol * nR0) and (nR > atol * nR):
+    x -= la.solve(J, R)
+    R, J = RJ(x)
+    nR = la.norm(R)
+    i += 1
+    if verbose:
+      print("%i\t%e\t%e\t" % (i, nR, nR / nR0))
+
+    if i > miter:
+      print("")
+      raise MaximumIterations()
+
+  print("")
+
+  return x
