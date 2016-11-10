2ceb80604367308ac43ca4ba4800937966b0ac77
Mark C. Messner <messner@anl.gov>
2016-11-10 11:45:50 -0600

    Brought ri_flow tests up to new standard

--- test/test_ri_flow.py
+++ test/test_ri_flow.py
@@ -13,44 +13,67 @@ class CommonFlowRule(object):
   """
     Tests common to all flow rules.
   """
+  def gen_stress(self):
+    s = ra.random((6,))
+    s = (1.0 - 2.0 * s) * 125.0
+    return s
+
   def test_history(self):
     self.assertEqual(self.model.nhist, len(self.hist0))
     self.assertTrue(np.allclose(self.model.init_hist(), self.hist0))
 
   def test_df_ds(self):
-    dfn = lambda s: self.model.f(s, self.hist_test, self.T_test)
-    num = differentiate(dfn, self.stress_test)
-    exact = self.model.df_ds(self.stress_test, self.hist_test, self.T_test)
+    stress = self.gen_stress()
+    hist = self.gen_hist()
+
+    dfn = lambda s: self.model.f(s, hist, self.T)
+    num = differentiate(dfn, stress)
+    exact = self.model.df_ds(stress, hist, self.T)
     self.assertTrue(np.allclose(num, exact, rtol = 1.0e-3))
 
   def test_df_da(self):
-    dfn = lambda a: self.model.f(self.stress_test, a, self.T_test)
-    num = differentiate(dfn, self.hist_test)
-    exact = self.model.df_da(self.stress_test, self.hist_test, self.T_test)
+    stress = self.gen_stress()
+    hist = self.gen_hist()
+
+    dfn = lambda a: self.model.f(stress, a, self.T)
+    num = differentiate(dfn, hist)
+    exact = self.model.df_da(stress, hist, self.T)
     self.assertTrue(np.allclose(num, exact, rtol = 1.0e-3))
 
   def test_dg_ds(self):
-    dfn = lambda s: self.model.g(s, self.hist_test, self.T_test)
-    num = differentiate(dfn, self.stress_test)
-    exact = self.model.dg_ds(self.stress_test, self.hist_test, self.T_test)
+    stress = self.gen_stress()
+    hist = self.gen_hist()
+
+    dfn = lambda s: self.model.g(s, hist, self.T)
+    num = differentiate(dfn, stress)
+    exact = self.model.dg_ds(stress, hist, self.T)
     self.assertTrue(np.allclose(num, exact, rtol = 1.0e-3))
 
   def test_dg_da(self):
-    dfn = lambda a: self.model.g(self.stress_test, a, self.T_test)
-    num = differentiate(dfn, self.hist_test)
-    exact = self.model.dg_da(self.stress_test, self.hist_test, self.T_test)
+    stress = self.gen_stress()
+    hist = self.gen_hist()
+
+    dfn = lambda a: self.model.g(stress, a, self.T)
+    num = differentiate(dfn, hist)
+    exact = self.model.dg_da(stress, hist, self.T)
     self.assertTrue(np.allclose(num, exact, rtol = 1.0e-3))
 
   def test_dh_ds(self):
-    dfn = lambda s: self.model.h(s, self.hist_test, self.T_test)
-    num = differentiate(dfn, self.stress_test)
-    exact = self.model.dh_ds(self.stress_test, self.hist_test, self.T_test)
+    stress = self.gen_stress()
+    hist = self.gen_hist()
+
+    dfn = lambda s: self.model.h(s, hist, self.T)
+    num = differentiate(dfn, stress)
+    exact = self.model.dh_ds(stress, hist, self.T)
     self.assertTrue(np.allclose(num, exact, rtol = 1.0e-3))
 
   def test_dh_da(self):
-    dfn = lambda a: self.model.h(self.stress_test, a, self.T_test)
-    num = differentiate(dfn, self.hist_test)
-    exact = self.model.dh_da(self.stress_test, self.hist_test, self.T_test)
+    stress = self.gen_stress()
+    hist = self.gen_hist()
+
+    dfn = lambda a: self.model.h(stress, a, self.T)
+    num = differentiate(dfn, hist)
+    exact = self.model.dh_da(stress, hist, self.T)
     self.assertTrue(np.allclose(num, exact, rtol = 1.0e-3))
 
 
@@ -65,18 +88,21 @@ class TestRateIndependentAssociativeFlowJ2Linear(unittest.TestCase, CommonFlowRu
 
     self.hist0 = np.zeros((1,))
     
-    self.stress_test = 100.0*ra.random((6,))
-    self.hist_test = np.abs(ra.random((1,)))
-    self.T_test = 300.0
+    self.T = 300.0
+
+  def gen_hist(self):
+    return ra.random((1,))
   
   def test_flow_rule(self):
-    dev_stress = self.stress_test - np.array([1,1,1,0,0,0]) * np.sum(self.stress_test[:3]) / 3.0
+    stress = self.gen_stress()
+    dev_stress = make_dev(stress)
     should_be = dev_stress / la.norm(dev_stress)
     
     self.assertTrue(np.allclose(should_be, 
-      self.model.g(self.stress_test, self.hist_test, self.T_test)))
+      self.model.g(stress, self.gen_hist(), self.T)))
 
   def test_hardening_rule(self):
     should_be = np.array([np.sqrt(2.0/3.0)])
     self.assertTrue(np.allclose(should_be,
-      self.model.h(self.stress_test, self.hist_test, self.T_test)))
+      self.model.h(self.gen_stress(), self.gen_hist(), self.T)))
+
