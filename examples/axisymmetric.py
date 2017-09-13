57b76c149cf14cddf235598620f03a794d2c53e9
Mark Messner <mark.messner@gmail.com>
2017-09-12 21:57:11 -0500

    Continued work

--- examples/axisymmetric.py
+++ examples/axisymmetric.py
@@ -3,11 +3,36 @@
 import sys
 sys.path.append('..')
 
-from neml import axisym
+from neml import axisym, neml, elasticity, surfaces
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+def gen_material(E, nu, sY, alpha):
+  """
+    Generate a perfectly plastic material model.
+  """
+  youngs = elasticity.YoungsModulus(E)
+  poissons = elasticity.PoissonsRatio(nu)
+  elastic = elasticity.IsotropicLinearElasticModel(youngs, poissons)
+  surface = surfaces.IsoJ2()
+
+  return neml.SmallStrainPerfectPlasticity(elastic, surface, sY)
 
 if __name__ == "__main__":
-  amodel = axisym.AxisymmetricProblem([10.0,15.0,25.0], [1,1], [10,10], 
-      lambda r, t: 0.0, lambda t: 0.0, bias = True)
+  tup = 10.0
+  gradient = axisym.generate_thickness_gradient(100.0, 250.0, 400, 600,
+      10.0, hold = 100.0, Tdot_cold = 20.0, hold_together = 20.0,
+      delay = tup)
+  p = 1.1
+  period = 150
+
+  amodel = axisym.AxisymmetricProblem([100.0,150.0,250.0], 
+      [gen_material(100000, 0.3, 100.0, 20.0e-6),
+        gen_material(150000, 0.35, 120.0, 15.0e-6)], [25,25], 
+      gradient, lambda t: p/tup * t, bias = True)
+
+  for t in np.linspace(0,tup+10*period, 4000):
+    amodel.step(t)
 
--- neml/axisym.py
+++ neml/axisym.py
@@ -2,12 +2,55 @@
 
 import numpy as np
 import numpy.linalg as la
+from numpy.polynomial.legendre import leggauss as lgg
 import scipy.optimize as opt
-from drivers import newton
 
-import matplotlib.pyplot as plt
+def generate_thickness_gradient(ri, ro, T1, T2, Tdot_hot, hold, 
+    Tdot_cold = None, hold_together = 0.0, delay = 0.0):
+  """
+    Generate our usual through-wall temperature gradient
 
-def mesh(rs, ns, bias = False, n = 4):
+    Parameters:
+      ri                inner radius
+      ro                outer radius
+      T1                initial temperature
+      T2                hot temperature
+      Tdot_hot          heating rate, also cooling rate if not specified
+      hold              hold at gradient
+
+    Optional:
+      Tdot_cold         cooling rate
+      hold_together     hold at no temperature gradient
+  """
+  if Tdot_cold is None:
+    Tdot_cold = Tdot
+
+  dT = T2 - T1
+  thot = np.abs(dT / Tdot_hot)
+  tcold = np.abs(dT / Tdot_cold)
+  period = thot + hold + tcold + hold_together
+
+  def gradient(r, t):
+    if t < delay:
+      return T1
+    t = (t-delay) % period
+
+    if t < thot:
+      Tright = T1 + t * Tdot_hot * np.sign(dT)
+    elif t < thot + hold:
+      Tright = T2
+    elif t < thot + hold + tcold:
+      Tright = T2 - (t - thot - hold) * Tdot_cold * np.sign(dT)
+    else:
+      return T1
+
+    xi = (r - ri) / (ro - ri)
+
+    return (1-xi) * T1 + xi * Tright
+
+  return gradient
+
+def mesh(rs, ns, bias = False, n = 2.0):
   if not bias:
     xpoints = [np.linspace(rs[0], rs[1], ns[0]+1)]
     for i in range(1, len(ns)):
@@ -43,20 +86,21 @@ class AxisymmetricProblem(object):
     Python driver for our axisymmetric reduced problem.
   """
   def __init__(self, rs, mats, ns, T, p, rtol = 1.0e-6, atol = 1.0e-8,
-      bias = False, n = 2.0):
+      bias = False, factor = 2.0, ngpts = 2):
     """
       Parameters:
         rs      radii deliminating each region
         mats    material model for each region
         ns      number of elements per region
         T       temperature as a function of (r, t)
-        p       pressure, as a function of (t)
+        factor  pressure, as a function of (t)
 
       Optional:
         rtol    relative tolerance for N-R solve
         atol    absolute tolerance for N-R solve
         bias    if true bias the mesh to the edges
         n       bias factor
+        ngpts   number of gauss points to use per element
     """
     # Check
     if len(rs) - 1 != len(mats):
@@ -77,15 +121,81 @@ class AxisymmetricProblem(object):
     self.t = np.sum(self.ts)
     self.r = rs[-1]
 
-    self.mesh = mesh(self.rs, self.ns, bias = bias, n = n)
-    
-    self.histories = []
-    self.histories.append([[m.init_hist() for i in range(n) for m,n in zip(
-      self.mats, self.ns)]])
+    self.mesh = mesh(self.rs, self.ns, bias = bias, n = factor)
+    self.nnodes = len(self.mesh)
+    self.nelem = len(self.mesh) - 1
 
+    self.materials = [m for m,n in zip(self.mats, self.ns) for i in range(n)]
+    
     self.times = [0.0]
-    self.pressures = [p(0)]
-    self.temperatures = [[T(xi, 0) for xi in self.mesh]]
+    self.pressures = [self.p(0)]
+    self.temperatures = [np.array([self.T(xi, 0) for xi in self.mesh])]
+    
+    self.displacements = [[0.0 for i in range(self.nnodes)]]
+    self.axialstrain = [0.0]
+    self.stresses = [[np.zeros((4,)) for n in self.ns for i in range(n)]]
+    self.strains = [[np.zeros((4,)) for n in self.ns for i in range(n)]]
+    self.mstrains = [[np.zeros((4,)) for n in self.ns for i in range(n)]]
+    self.estrains = [[np.zeros((4,)) for n in self.ns for i in range(n)]]
+
+    self.gpoints, self.gweights = lgg(ngpts)
+
+    self.histories = [[[m.init_store() for xi in self.gpoints]
+      for m in self.materials]]
+
+    self.ls = np.diff(self.mesh)
+
+    # Save a bit of time
+    self.ri = np.array([[(xi+1)/2*li + ri for xi in self.gpoints] for li, ri 
+      in zip(self.ls, self.mesh)])
+    self.Nl = np.array([[(1-xi)/2, (1+xi)/2] for xi in self.gpoints])
+    self.Bl = np.array([[-1, 1] for xi in self.gpoints])
 
   def step(self, t):
-    pass
+    """
+      Update to the next state
+
+      Parameters:
+        t       time requested
+    """
+    T = np.array([self.T(xi, t) for xi in self.mesh])
+    p = self.p(t)
+    dt = t - self.times[-1]
+    
+    # Get a guess
+    x0 = np.concatenate((self.displacements[-1], [self.axialstrain[-1]]))
+
+    R0 = self.R(x0, T, p)
+
+    self.times.append(t)
+    self.pressures.append(p)
+    self.temperatures.append(T)
+
+  def R(self, x, T, p):
+    """
+      Compute the residual and the updated history
+
+      Parameters:
+        x       iterate
+        T       nodal temperatures
+        p       left pressure
+    """
+    d = x[:-1]
+    ez = x[-1]
+    
+    Fext = np.zeros((self.nnodes+1,))
+    Fext[0] = self.rs[-1] * p
+    Fext[-1] = p * self.r / self.t
+
+    Fint = np.zeros((self.nnodes+1,))
+    for e, (mat, l, rs) in enumerate(zip(self.materials, self.ls, self.ri)):
+      de = d[e:e+2]
+      Te = T[e:e+2]
+      Tp = self.temperatures[-1][e:e+2]
+      dT = Te - Tp
+      err = np.dot(self.Bl, de) * 2.0 / l
+      ett = np.dot(self.Nl, de) / rs
+      ezz = np.array([ez, ez])
+      strain = np.vstack((err, ett, ezz)).T
+
+    return Fint - Fext
