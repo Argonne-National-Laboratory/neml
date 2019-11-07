9c8012e4c08942c31214a992d024a224a87a237f
Mark Messner <mark.messner@gmail.com>
2019-11-07 16:05:05 -0600

    Updated pybind11 version and compile system

--- src/macros.cmake
+++ src/macros.cmake
@@ -1,14 +1,6 @@
 ### Macro helper for python bindings ###
 macro(pybind sname)
       set(wrapper "${sname}_wrap.cxx")
-      add_library(${sname} SHARED ${wrapper})
-      target_link_libraries(${sname} neml ${PYTHON_LIBRARIES})
-      set_target_properties(${sname} PROPERTIES PREFIX "")
-      if (APPLE)
-            set_property(TARGET ${sname} PROPERTY OUTPUT_NAME "${sname}.so")
-            set_property(TARGET ${sname} PROPERTY SUFFIX "")
-      endif()
-      if (WIN32)
-
-      endif()
+      pybind11_add_module(${sname} SHARED ${wrapper})
+      target_link_libraries(${sname} PRIVATE neml)
 endmacro(pybind)
