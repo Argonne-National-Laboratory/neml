bc7f8de075980c8ff278b9bba3b587064c7b015f
Lynn Munday <lynn.munday@inl.gov>
2020-05-20 16:51:25 -0600

    Change interface for xml in string format so that it will also accept a string for the model name

--- src/neml_interface.h
+++ src/neml_interface.h
@@ -8,7 +8,7 @@
 
 namespace neml {
 std::unique_ptr<NEMLModel> parse_xml_unique(std::string fname, std::string mname);
-std::unique_ptr<NEMLModel> parse_string_unique(std::string input);
+std::unique_ptr<NEMLModel> parse_string_unique(std::string input, std::string mname);
 }
 
 #endif // NEML_INTERFACE_H
