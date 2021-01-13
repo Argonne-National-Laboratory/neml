1ad48b765bbb005851e143148924797b4628d3cf
Mark Messner <mark.messner@gmail.com>
2021-01-13 14:36:26 -0600

    Changed name to NEML_STRAIN_RATE_LIMIT

--- src/general_flow.cxx
+++ src/general_flow.cxx
@@ -80,7 +80,7 @@ int TVPFlowRule::s(const double * const s, const double * const alpha,
   if (ier != SUCCESS) return ier;
   ier = flow_->y(s, alpha, T, yv);
   if (ier != SUCCESS) return ier;
-  if (yv > STRAIN_RATE_LIMIT) return EXCEEDS_STRAIN_RATE_LIMIT;
+  if (yv > NEML_STRAIN_RATE_LIMIT) return EXCEEDS_STRAIN_RATE_LIMIT;
   
   for (int i=0; i<6; i++) {
     erate[i] -= yv * temp[i];
