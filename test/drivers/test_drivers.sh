add5d76f4214e63be62d01d2f9fa4103b08fc6fa
= <mark.messner@gmail.com>
2021-06-25 18:03:50 -0500

    Better error checking and test to figure out path issue

--- test/drivers/test_drivers.sh
+++ test/drivers/test_drivers.sh
@@ -6,13 +6,22 @@ for d in ../test_regression/test_*/
 do
 	echo "Beginning $(basename $d)"
 	printf "\tTesting cxx driver..."
-	../../util/cxx_interface/cxxsimple "$d/model.xml" "model" 0.1 100.0 100 300
+	if ! ../../util/cxx_interface/cxxsimple "$d/model.xml" "model" 0.1 100.0 100 300; then
+		printf " error!\n"
+		exit -1
+	fi
 	printf " done\n"
 	printf "\tTesting c driver..."
-	../../util/c_interface/csimple "$d/model.xml" "model" 0.1 100.0 100 300
+	if ! ../../util/c_interface/csimple "$d/model.xml" "model" 0.1 100.0 100 300; then
+		printf " error!\n"
+		exit -1
+	fi
 	printf " done\n"
 	printf "\tTesting fortran driver..."
-	../../util/f_interface/fsimple "$d/model.xml" "model" 0.1 100.0 100 300
+	if ! ../../util/f_interface/fsimple "$d/model.xml" "model" 0.1 100.0 100 300; then
+		printf " error!\n"
+		exit -1
+	fi
 	printf " done\n"
 	echo ""
 done
