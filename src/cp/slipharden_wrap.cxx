55d8b686a2071493891ecf1e2719a0a77379e770
tianjuchen <chentianju@gmail.com>
2021-11-18 23:20:59 -0500

    reparameterize lanltimodel

--- src/cp/slipharden_wrap.cxx
+++ src/cp/slipharden_wrap.cxx
@@ -74,8 +74,8 @@ PYBIND11_MODULE(slipharden, m) {
       .def(py::init([](py::args args, py::kwargs kwargs)
                     {
                       return create_object_python<LANLTiModel>(
-                          args, kwargs, {"tau_0", "C_st", "mu_s", "mu_t", 
-						  "k1", "k2", "b_s", "b_t"});
+                          args, kwargs, {"tau_0", "C_st", "mu", 
+						  "k1", "k2"});
                     }))
       ;
 
