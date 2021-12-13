c2130f03baa3355ed20c096e529467cc205b6d82
tianjuchen <chentianju@gmail.com>
2021-12-13 15:06:36 -0500

    add simplify Ti model

--- examples/cp/Ti_uniaxial_fitting.py
+++ examples/cp/Ti_uniaxial_fitting.py
@@ -23,6 +23,112 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+def simplify_model(X_s, k1_1, k1_2, k1_3, X, 
+            k2_1, k2_2, k2_3,
+            T = 298.0, emax = 0.05, N = 1, 
+            strain_rate = 1.0e-4, nthreads = 1, 
+            verbose = True, Taylor = True, PTR = True):  
+  # Model
+  a = 2.9511*0.1 # nm
+  c = 4.68433*0.1 # nm
+  
+  # Elastic constants in MPa
+  C11 = 160000.0
+  C33 = 181000.0
+  C44 = 46500.0
+  C12 = 90000.0
+  C13 = 66000.0
+  
+  # Constant part of the strength for slip and twin
+  tau0 = np.array([170.0]*3+[90.5]*3+[210]*6+[180.0]*6+[250.0]*6)
+  
+  # Reference slip rate and rate sensitivity exponent
+  g0 = 1.0
+  n = 12.0
+  
+  # Twin threshold 
+  twin_threshold = 0.75
+  
+  # Sets up the lattice crystallography
+  lattice = crystallography.HCPLattice(a, c)
+  # Basal <a>
+  lattice.add_slip_system([1,1,-2,0],[0,0,0,1])
+  # Prismatic <a>
+  lattice.add_slip_system([1,1,-2,0],[1,0,-1,0])
+  # Pyramidal <c+a>
+  lattice.add_slip_system([1,1,-2,-3],[1,1,-2,2])
+  # Tension twinning
+  lattice.add_twin_system([-1,0,1,1],[1,0,-1,2],[1,0,-1,1],[1,0,-1,-2])
+  # Compression twinning
+  lattice.add_twin_system([1,1,-2,-3],[1,1,-2,2],[2,2,-4,3],[1,1,-2,-4])
+  
+  # Sets up the interaction matrix
+  num_basal, num_prism, num_pyram = 3, 3, 6
+  num_ttwin, num_ctwin = 6, 6
+  ## basal plane
+  C1_single = np.array([50.0]*num_ttwin+[50.0]*num_ttwin)
+  C1 = np.stack([C1_single for _ in range(num_basal)])
+  ## prismatic plane
+  C2_single = np.array([100.0]*num_ttwin+[1000.0]*num_ttwin)
+  C2 = np.stack([C2_single for _ in range(num_prism)])
+  ## pyramidal plane
+  C3_single = np.array([250.0]*num_ttwin+[170.0]*num_ttwin)
+  C3 = np.stack([C3_single for _ in range(num_pyram)])
+  ## stack up each slip system
+  C_np = np.vstack((C1, C2, C3)).T
+
+  C_st = matrix.SquareMatrix(12, type = "dense", data = C_np.flatten())
+
+  mu = np.ones((24,))
+  mu_slip = 30000.0
+  mu_twin = 25000.0
+  mu[:12] = mu_slip
+  mu[12:] = mu_twin
+
+
+  k1 = np.array([k1_1]*3+[k1_2]*3+[k1_3]*6)
+
+  k2 = np.array([k2_1]*3+[k2_2]*3+[k2_3]*6)
+  
+  # Sets up the linear elastic tensor
+  emodel = elasticity.TransverseIsotropicLinearElasticModel(
+      C11,C33,C12,C13,C44,"components")
+  
+  # Sets up the slip system strength model (this is what you'll change)
+  strength = slipharden.LANLTiModel(tau0, C_st, mu, k1, k2, X_s=X_s)
+  # strength = slipharden.SimpleLinearHardening(M, tau0)
+  # Sets up the slip rule
+  slipmodel = sliprules.PowerLawSlipRule(strength, g0, n)
+  # Sets up the model inelastic rate kinematics 
+  imodel = inelasticity.AsaroInelasticity(slipmodel)
+  # Sets up the overall model kinematics
+  kmodel = kinematics.StandardKinematicModel(emodel, imodel)
+  
+  # This is the object that causes twins to recrystallize
+  twinner = postprocessors.PTRTwinReorientation(twin_threshold)
+  
+  # Sets up the single crystal model
+  if PTR:
+    single_model = singlecrystal.SingleCrystalModel(kmodel, lattice, 
+        postprocessors = [], verbose = False, linesearch = True,
+        initial_rotation = rotations.Orientation(0,0,0,angle_type="degrees"),
+        miter = 100, max_divide = 10)
+  else:
+    single_model = singlecrystal.SingleCrystalModel(kmodel, lattice, 
+        verbose = False, linesearch = True,
+        initial_rotation = rotations.Orientation(0,0,0,angle_type="degrees"),
+        miter = 100, max_divide = 10)
+        
+  if Taylor:
+    orientations = rotations.random_orientations(N)
+    model = polycrystal.TaylorModel(single_model, orientations, nthreads = nthreads)
+    return drivers.uniaxial_test(model, strain_rate, T = T, 
+            emax = emax, verbose = verbose)
+  else:
+    return drivers.uniaxial_test(single_model, strain_rate, 
+            T = T, emax = emax, verbose = verbose)
+
+
 def make_model(X_s, k1_1, k1_2, k1_3, X, 
             g_1, g_2, g_3,
             tau_D1, tau_D2, tau_D3,
