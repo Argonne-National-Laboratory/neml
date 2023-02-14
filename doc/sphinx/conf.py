674febfe2eee4c2990601a046e00c0023211f77b
Mark Messner <mark.messner@gmail.com>
2023-02-14 16:10:12 -0600

    Increment version number

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -32,9 +32,9 @@ copyright = u'2023, UChicago Argonne, LLC'
 author = u'Argonne National Laboratory'
 
 # The short X.Y version
-version = u'1.4.1'
+version = u'1.5.2'
 # The full version, including alpha/beta/rc tags
-release = u'1.4.1'
+release = u'1.5.2'
 
 
 # -- General configuration ---------------------------------------------------
