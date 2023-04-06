71607afa0d316bf915eb1a5c4536295e98db771b
Mark Messner <mark.messner@gmail.com>
2023-04-05 20:54:45 -0500

    Fixing imgmath I hope

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -53,6 +53,8 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+imgmath_latex_preamble = '\\usepackage{bm}'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -70,7 +72,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
